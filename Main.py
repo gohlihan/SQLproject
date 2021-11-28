d65ac254d2a2a0c3052966035c4231527d3e501b
gohlihan <han_0607@msn.com>
2021-11-29 01:10:49 +0800

    done requirement 1, updated README

--- Main.py
+++ Main.py
@@ -2,6 +2,21 @@ import DbFunction as dbf
 from tkinter import *
 
 
-window = Tk()
-window.geometry("500x500")
-window.mainloop()
+#-----------Tkinter testing---------------------------
+#window = Tk()
+#window.geometry("500x500")
+#window.mainloop()
+
+
+
+#----------Main functions--------------------
+
+#dbf.show_warehouse_inv(1)
+
+#dbf.show_all_warehouse_inv()
+
+#dbf.change_warehouse_inv(99,1)
+
+#dbf.change_invoice_city(5,3)
+
+#dbf.create_invoice(invoice_id,invoice_date,customer_id,shopping_list_id,total_price,city_id,driver_id,shipping_status)
